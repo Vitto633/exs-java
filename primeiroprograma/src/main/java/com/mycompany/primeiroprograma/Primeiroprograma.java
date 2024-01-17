426e8217626ddae05f1ef9befa6f732c0ee2e34d
Vitor da Luz Santos <142545890+Vitto633@users.noreply.github.com>
2024-01-17 16:08:32 -0300

    Primeiro programa em java

---
+++ primeiroprograma/src/main/java/com/mycompany/primeiroprograma/Primeiroprograma.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.primeiroprograma;
+
+public class Primeiroprograma {
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
+}
