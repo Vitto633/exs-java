269a541feeae2ee878bd62e09a58c2b300d958c3
Vitor da Luz Santos <142545890+Vitto633@users.noreply.github.com>
2024-01-17 21:38:45 -0300

    Mostrar a data do sistema em Java

---
+++ ProgramaData/src/programadata/ProgramaData.java
@@ -0,0 +1,18 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programadata;
+
+import java.util.Date;
+
+public class ProgramaData {
+    
+    public static void main(String[] args) {
+        System.out.println("A data do sistema e:");
+        Date relogio = new Date();
+        System.out.println(relogio.toString());
+    }
+    
+}
