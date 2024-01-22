9652f4f8157c18c5888163d3a17d52a45df5b50b
Vitor da Luz Santos <142545890+Vitto633@users.noreply.github.com>
2024-01-22 18:21:51 -0300

    Mostra a média de dois números preenchidos.

---
+++ ProgramaOperadoresAritmeticos/src/programaoperadoresaritmeticos/ProgramaOperadoresAritmeticos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programaoperadoresaritmeticos;
+
+/**
+ *
+ * @author Pichau
+ */
+public class ProgramaOperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1 = 20;
+        int n2 = 2;
+        
+        float media = (n1 + n2)/2;
+        
+        System.out.printf("A media eh: %.0f \n", media);
+    }
+    
+}
