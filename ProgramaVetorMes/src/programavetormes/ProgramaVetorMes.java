90dd8ef2877cabf0822243487c862782150f569e
Vitor da Luz Santos <142545890+Vitto633@users.noreply.github.com>
2024-02-02 18:48:16 -0300

    Fix: melhorado o exercício 30 adicionando condição para ano bissexto

--- ProgramaVetorMes/src/programavetormes/ProgramaVetorMes.java
+++ ProgramaVetorMes/src/programavetormes/ProgramaVetorMes.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package programavetormes;
-
+import java.util.Date;
+import java.util.GregorianCalendar;
 /**
  *
  * @author Pichau
@@ -15,9 +16,15 @@ public class ProgramaVetorMes {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GregorianCalendar calendario = new GregorianCalendar();
+        Date dataSistema = new Date();
+        int dataAtual = dataSistema.getYear() + 1900;
         String meses[] = {"Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
         String dias[] = {"31", "28", "31", "30","31","30","31","31","30","31","30","31"};
         
+        if(calendario.isLeapYear(dataAtual) == true){
+            dias[1] = "29";
+        }
         for(int i = 0; i<meses.length;i++){
             System.out.println("O mes "+meses[i]+" tem "+dias[i]+" dias");
         }
